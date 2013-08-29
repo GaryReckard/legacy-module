d6e412af6e6e462218bd88079a7b67ad737fa4a3
Ryan Barrington Cox <ryanbarrington@gmail.com>
2013-08-29 14:29:43 -0400

    allows for running updates multiple times

--- mcp.legacy.php
+++ mcp.legacy.php
@@ -33,8 +33,7 @@ class Legacy_mcp {
 	/**
 	 * Constructor
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 		$this->EE =& get_instance();
 		
 		$this->_base_url = BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module=legacy';
@@ -52,8 +51,7 @@ class Legacy_mcp {
 	 *
 	 * @return 	void
 	 */
-	public function index()
-	{
+	public function index() {
 		$this->EE->load->library('table');
 		
 		$this->EE->view->cp_page_title = 'Legacy';
@@ -69,6 +67,22 @@ class Legacy_mcp {
 			"Import tags from string to tagger field",
 		);
 		return $this->EE->load->view('index.php', $vars, TRUE);
+	}
+	
+	public function getPageMap() {
+				
+		$this->EE->db->select('*');
+		$this->EE->db->from('exp_sites');
+		$this->EE->db->where('site_id',1);		
+		$query = $this->EE->db->get();
+
+		$result = $query->result_array();
+		$site_pages = $result[0]['site_pages'];
+		$site_pages = base64_decode($site_pages);
+		$page_map = unserialize($site_pages);
+		
+		//die(print_r($page_map[1]['uris']));
+		return $page_map[1]['uris'];
 	}
 
 	public function showShortcodeOptions() {
@@ -87,6 +101,7 @@ class Legacy_mcp {
 		$pattern = null;
 		$starts_with = null;
 		$vars['candidates'] = array();
+		$page_map = $this->getPageMap();
 
 		//query db for shortcode candidates: entry_id, blog or page type, title with link
 		$query_string = "SELECT * FROM exp_channel_data, exp_channel_titles WHERE exp_channel_data.entry_id = exp_channel_titles.entry_id";
@@ -96,15 +111,15 @@ class Legacy_mcp {
 		
 		foreach ($entries as $entry) {
 			
+			$entry_id = $entry['entry_id'];
+			
 			//is it a blog or page?  if neither then disregard
 			if ($entry['channel_id'] == 1) {
 				$type = "page";
 				$text = $entry['field_id_1'];
-				$url = $this->EE->functions->fetch_site_index().QUERY_MARKER.'URL='.urlencode($this->EE->functions->create_url($entry['url_title']));
 			} else if ($entry['channel_id'] == 3) {
 				$type = "blog";
 				$text = $entry['field_id_13'];
-				$url = "/index.php/blog/entry/".$entry['url_title'];
 			} else continue;
 			
 			//look for []'s in body
@@ -114,48 +129,51 @@ class Legacy_mcp {
 			//if we have an opening '[' add to candidates
 			if ($matches != NULL) {
 				$str_arr = explode(' ',trim($matches[0]));
-				$starts_with = $str_arr[0];		
+				$starts_with = $str_arr[0];
+				if ($type == "page") {
+					$url = $page_map[$entry_id];
+				} else {
+					$url = "/index.php/blog/entry/".$entry['url_title'];
+				}
 				$arr =  array('entry_id'=>$entry['entry_id'], 'type'=>$type, 'starts_with'=>$starts_with, 'title'=>$entry['title'], 'url'=>$url);
 				array_push($vars['candidates'], $arr);
 			}				
 		}	
-
+		
 		return $this->EE->load->view('shortcode-view.php', $vars, TRUE);
 	}
 
-	public function redirectBlogs() {
+	//get a map of existing legacy_urls and their id
+	public function getLegacyMap($query_string) {
+		//die($query_string);
 		
-		$this->EE->view->cp_page_title = '301 Redirect Blogs';
-		$this->EE->cp->set_breadcrumb($this->_base_url, "Legacy");
-		
-		$blog_channel_id = 3;
-		$blog_legacy_url = 'field_id_17';
-		$blog_path = 'blog/entry/';
-		$vars['inserted_blog_redirects'] = array();
-		$vars['updated_blog_redirects'] = array();
 		$legacy_url_map = array();
 		
-		//get a map of existing legacy_urls and their id
-		$query_string = "SELECT detour_id, original_url FROM exp_detours";
-		//die($query_string);
 		$query = $this->EE->db->query($query_string);
 		$legacy_entries = $query->result_array();	
 		foreach ($legacy_entries as $legacy_entry) {
 			$legacy_url_map[$legacy_entry['original_url']] = $legacy_entry['detour_id'];
 		}
-		///die("<pre>".print_r($legacy_url_map,true)."</pre>");
+		//die("<pre>".print_r($legacy_url_map,true)."</pre>");
+		return $legacy_url_map;	
+	}
+
+	public function redirectHelper($results, $channel_id, $legacy_url) {
 		
-		//get url-title and legacy-url from all blogs
-		$query_string = "SELECT $blog_legacy_url as legacy_url, url_title, ct.channel_id FROM exp_channel_data cd, exp_channel_titles ct
-			WHERE cd.entry_id = ct.entry_id and ct.channel_id = 3";
-		//die($query_string);
-		$query = $this->EE->db->query($query_string);
-		$results = $query->result_array();
+		$this->EE->view->cp_page_title = '301 Redirect Blogs';
+		$this->EE->cp->set_breadcrumb($this->_base_url, "Legacy");		
+	
+		$vars['inserted_redirects'] = array();
+		$vars['updated_redirects'] = array();
 		
+		$map_query_string = "SELECT detour_id, original_url FROM exp_detours";	
+		$legacy_map = $this->getLegacyMap($map_query_string);
+		
+		$domainless_legacy_url = "";
 		//insert the pair, along with 301 option into exp_detours
-		//die("results count is ".count($results));
 		foreach($results as $row) {
-			$new_url = $blog_path.$row['url_title'];
+			
+			$new_url="";	
 			$old_url_segments = parse_url($row['legacy_url']);
 			$domainless_legacy_url = $old_url_segments['path'];
 			$domainless_legacy_url = ltrim($domainless_legacy_url, "/");
@@ -163,32 +181,98 @@ class Legacy_mcp {
 			
 			//strip all the stuff after a % legacy, replace with one % b/c detour uses this as a wildcard
 			$arr = explode("%",$domainless_legacy_url);
+			$legacy_url_no_wildcard = $arr[0];
 			$domainless_legacy_url = $arr[0]."%";
 			
+			//if it's a blog
+			if ($row['channel_id'] == 3) {			
+				$path = 'blog/entry/';
+				$new_url = $path.$row['url_title'];			
+			}
+			
+			//else if it's a page
+			elseif ($row['channel_id'] == 1) {
+				//echo "entry_id is ".$row['entry_id']."<br>";
+				$page_map = $this->getPageMap();
+				$entry_id = $row['entry_id'];
+				//print_r($page_map);
+				//die($page_map($row['entry_id']));
+				$new_url = $page_map[$entry_id];
+				$new_url = ltrim($new_url, "/");
+				//die($new_url);
+			}
+			
+			else {
+				die("Something's gone terribly wrong.  Call for help!");
+			}
+			
+			//special case: urls are same which causes infinte redirect loop, don't insert if they're the same!
+			if ($new_url ==$legacy_url_no_wildcard) {
+				
+				//remove orignal url redirect if exists
+				$this->EE->db->delete('exp_detours', array('original_url'=>$domainless_legacy_url));
+				echo "removed entry";
+				//break out of loop...
+				continue;
+			}
+			
 			//if the legacy url is already in there, update the entry
-			if (array_key_exists($domainless_legacy_url, $legacy_url_map)) {
-				$data = array('new_url' => $new_url, 'detour_method' => 301, 'site_id' => 1);				
-				$this->EE->db->where('original_url', $legacy_url_map[$domainless_legacy_url]);
-				$this->EE->db->update('exp_detours', $data); 
-				array_push($vars['updated_blog_redirects'], $data);
+			if (array_key_exists($domainless_legacy_url, $legacy_map)) {
+				
+				$detour_id = $legacy_map[$domainless_legacy_url];
+				$data = array('original_url' => $domainless_legacy_url, 'new_url' => $new_url, 'detour_method' => 301, 'site_id' => 1);			
+				$this->EE->db->where('detour_id', $detour_id);
+				$this->EE->db->update('exp_detours', $data);
+				array_push($vars['updated_redirects'], $data);
 			}
 			//else insert as new
 			else {
 				$data = array('original_url' => $domainless_legacy_url, 'new_url' => $new_url, 'detour_method' => 301, 'site_id' => 1);
 				$sql = $this->EE->db->insert_string('exp_detours', $data);
 				$sql = $this->EE->db->query($sql);
-				array_push($vars['inserted_blog_redirects'], $data);				
+				array_push($vars['inserted_redirects'], $data);	
 			}
 		}
 		
-		return $this->EE->load->view('redirections-results.php', $vars, TRUE);
+		return $vars;	
+	}
+
+	public function redirectBlogs() {
+
+		$blog_channel_id = 3;
+		$blog_legacy_url = 'field_id_17';
+		
+		//get url-title and legacy-url from all blogs
+		$query_string = "SELECT cd.channel_id, $blog_legacy_url as legacy_url, url_title, ct.channel_id FROM exp_channel_data cd, exp_channel_titles ct
+			WHERE cd.entry_id = ct.entry_id and ct.channel_id = 3 AND $blog_legacy_url <> ''";
+		//die($query_string);
+		$query = $this->EE->db->query($query_string);
+		$results = $query->result_array();		
+		
+		$vars = $this->redirectHelper($results, $blog_channel_id, $blog_legacy_url);
+		
+		return $this->EE->load->view('redirections-results.php', $vars, TRUE);	
+		
 	}
 	
 	public function redirectPages() {
+		
+		$page_channel_id = 1;
+		$page_legacy_url = 'field_id_21';
+		
+		//get url-title and legacy-url from all blogs
+		$query_string = "SELECT cd.entry_id, cd.channel_id, $page_legacy_url as legacy_url, url_title, ct.channel_id FROM exp_channel_data cd, exp_channel_titles ct
+			WHERE cd.entry_id = ct.entry_id and ct.channel_id = $page_channel_id AND  $page_legacy_url <> ''";
+			
+		$query = $this->EE->db->query($query_string);
+		$results = $query->result_array();	
 			
 		$this->EE->view->cp_page_title = '301 Redirect Pages';
 		$this->EE->cp->set_breadcrumb($this->_base_url, "Legacy");
 		
+		$vars = $this->redirectHelper($results, $page_channel_id, $page_legacy_url);
+		
+		return $this->EE->load->view('redirections-results.php', $vars, TRUE);	
 		
 	}
 	
--- views/redirections-results.php
+++ views/redirections-results.php
@@ -1,7 +1,7 @@
 <p><?=lang('inserted_redirects_label')?></p>
 
-<pre><?=print_r($inserted_blog_redirects,true)?></pre>
+<pre><?=print_r($inserted_redirects,true)?></pre>
 
 <p><?=lang('updated_redirects_label')?></p>
 
-<pre><?=print_r($updated_blog_redirects,true)?></pre>
+<pre><?=print_r($updated_redirects,true)?></pre>
